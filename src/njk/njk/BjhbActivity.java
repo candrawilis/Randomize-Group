2c306c4ab05582054b4d63c931db6d0080b2c39c
Muh. Zaenul Hilmi <jejebawang@gmail.com>
2012-08-31 23:15:11 +0800

    hj

---
+++ src/njk/njk/BjhbActivity.java
@@ -0,0 +1,13 @@
+package njk.njk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BjhbActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
